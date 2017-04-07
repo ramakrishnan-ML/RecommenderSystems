839e7a80b56f8a65245fd2720c9fb2b1786d81ec
RAMAKRISHNAN RADHAKRISHNAN <r_ramakrishnan@hotmail.com>
2017-04-07 12:25:01 +0200

    Added multiple recommendations in given parameters

--- demoMain.py
+++ demoMain.py
@@ -1,17 +1,12 @@
 # Use xlrd for the time being. Then use pandas when you know.
+import sys
 from xlrd import open_workbook
-
-
-margErrTable = open_workbook('LEDsafari Analysis.xlsx')
-global margErrRed
-margErrRed = []
-global headers
-headers = []
-
-
 def marginalErrorReading():
     for sheet in margErrTable.sheets():
         if sheet.name == 'Sheet2':
+            global margRows, margCols
+            margRows = sheet.nrows
+            margCols = sheet.ncols
             for col in range(sheet.ncols):
                 values = [] ## Localised values for a row
                 for row in range(sheet.nrows):
@@ -22,7 +17,6 @@ def marginalErrorReading():
                         values.append(data)
                 if(row > 0):
                     margErrRed.append(values)
-    print(margErrRed)
 
 def pickLeastValues():
     leastValues = [] # Pick least values in each params.
@@ -32,7 +26,7 @@ def pickLeastValues():
 
 def pickLeastParams(minValuesList):
     minValue = min(minValuesList)
-    print(minValue)
+    #print(minValue)
     ##Find the position for it.
     global minParam
     minParam = minValuesList.index(minValue)
@@ -44,19 +38,33 @@ def pickLeastParams(minValuesList):
         if(paramTempPos == minParam):
             minParamValuePos = value.index(minValue) + 1 ## SInce the position starts from 0
             paramTempPos = paramTempPos + 1
-            #TODO: Over writing the values once used
-            #for eachValue in value:
-             #   margErrRed.
         else:
             paramTempPos = paramTempPos + 1
     recoPercentage = (minParamValuePos * 10)
     return recoPercentage
 
+def overWriting(colToBeAltered):
+    ## Alteration takes place in the column.
+    ## Step 1: Find out the position of margErrRed.
+
+    for i in range(margRows - 1):
+        margErrRed[colToBeAltered - 1][i] = sys.maxsize
+
+margErrTable = open_workbook('LEDsafari Analysis.xlsx')
+global margErrRed
+margErrRed = []
+global headers
+headers = []
 marginalErrorReading()
-minValues = pickLeastValues()
-print(minValues)
-recoPercentage = pickLeastParams(minValues)
-print ("The recommendation percentage is ", recoPercentage, "in Parameter : ", minParam )
+totalParameters = 3 ## Hardcode of parameters based on the no.of parameters chosen.
+for i in range(totalParameters):
+    minValues = pickLeastValues()
+    #print(minValues)
+    recoPercentage = pickLeastParams(minValues)
+    print ("The recommendation percentage is ", recoPercentage, "in Parameter : ", minParam )
+    if(i != totalParameters - 1): ## Since i iterates from 0.
+        print("Or")
+    overWriting(minParam)
 
 
 
